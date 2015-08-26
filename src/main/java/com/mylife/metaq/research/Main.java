7da2ba121ea5879d7e94ef9a34f0bc6b60a41a09
zhangyong <zhangyong7120180@163.com>
2015-08-26 15:45:33 +0800

    我对于metaq的理解

---
+++ src/main/java/com/mylife/metaq/research/Main.java
@@ -0,0 +1,16 @@
+package com.mylife.metaq.research;
+
+/**
+ * 主函数
+ * User: zhangyong
+ * Date: 2015/8/26
+ * Time: 15:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+
+}
