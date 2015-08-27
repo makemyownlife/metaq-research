e8a914bd035692d1d720c7bdc21eaa6031097d46
zhangyong <zhangyong7120180@163.com>
2015-08-27 12:41:54 +0800

    分区 生产者

---
+++ src/main/java/com/mylife/metaq/research/office/ProducerWithPartition.java
@@ -0,0 +1,16 @@
+package com.mylife.metaq.research.office;
+
+/**
+ * 生产者使用分区
+ * User: zhangyong
+ * Date: 2015/8/27
+ * Time: 10:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProducerWithPartition {
+
+    public static void main(String[] args) {
+
+    }
+
+}
