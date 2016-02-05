f1346ae4c126f05a75b578d441a4f8a4b3e71388
zhangyong <zhangyong7120180@163.com>
2016-02-05 15:27:56 +0800

    rocketmq 的测试

---
+++ src/main/java/com/mylife/metaq/research/rocketmq/RocketMqListener.java
@@ -0,0 +1,14 @@
+package com.mylife.metaq.research.rocketmq;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangyong
+ * Date: 2016/2/5
+ * Time: 15:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class RocketMqListener {
+
+
+
+}
