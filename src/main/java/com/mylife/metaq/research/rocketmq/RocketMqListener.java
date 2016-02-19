1ecf4fb60d0cf8486fc958597acd434b0aaf695d
zhangyong <zhangyong7120180@163.com>
2016-02-19 14:26:13 +0800

    rocketmq 的简单的例子

--- src/main/java/com/mylife/metaq/research/rocketmq/RocketMqListener.java
+++ src/main/java/com/mylife/metaq/research/rocketmq/RocketMqListener.java
@@ -1,5 +1,15 @@
 package com.mylife.metaq.research.rocketmq;
 
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: zhangyong
@@ -9,6 +19,27 @@ package com.mylife.metaq.research.rocketmq;
  */
 public class RocketMqListener {
 
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("MY_GROUP");
+        consumer.setNamesrvAddr("10.100.19.144:9876");
+        /**
+         * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费<br>
+         * 如果非第一次启动，那么按照上次消费的位置继续消费
+         */
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("TopicTest", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
 
+        consumer.start();
+
+        System.out.println("Consumer Started.");
+    }
 
 }
--- src/main/java/com/mylife/metaq/research/rocketmq/RocketMqProducer.java
+++ src/main/java/com/mylife/metaq/research/rocketmq/RocketMqProducer.java
@@ -1,5 +1,10 @@
 package com.mylife.metaq.research.rocketmq;
 
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+
 /**
  * Created with IntelliJ IDEA.
  * User: zhangyong
@@ -9,6 +14,25 @@ package com.mylife.metaq.research.rocketmq;
  */
 public class RocketMqProducer {
 
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+        DefaultMQProducer producer = new DefaultMQProducer("MY_GROUP");
+        producer.setNamesrvAddr("10.100.19.144:9876");
+        producer.start();
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Message msg = new Message("TopicTest",
+                        "TagA",
+                        ("Hello RocketMQ " + i).getBytes()
+                );
+                SendResult sendResult = producer.send(msg);
+                System.out.println(sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
 
+        producer.shutdown();
+    }
 
 }
